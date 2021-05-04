b750ab2eceeb55d2da14ec5e5cd82ec2e77bad84
Niraj Patel <nirajp82@yahoo.com>
2021-05-04 19:46:36 -0400

    Completed Code Deploy

---
+++ MicroService.Advert.API/Service/MockAdvertStorage.cs
@@ -0,0 +1,26 @@
+﻿using MicroService.Advert.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroService.Advert.API.Service
+{
+    public class MockAdvertStorage : IAdvertStorageService
+    {
+        public Task<string> Add(AdvertModel model)
+        {
+            return Task.FromResult(Guid.NewGuid().ToString());
+        }
+
+        public Task<bool> CheckHealthAsync()
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
